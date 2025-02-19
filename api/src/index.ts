d2e2d831509f8d98dd3b6eb68bae697d5a8d84f6
Iago Gouveia <iago.gouveia@w16.dev>
2025-02-18 21:46:35 -0300

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,19 @@
-import productsRouters from './routes/products'
+import { json, urlencoded, Request, Response } from "express";
+import productsRouters from "./routes/products";
 
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+app.use(urlencoded({ extended: false }));
+app.use(json());
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.use('/products', productsRouters);
+app.use("/products", productsRouters);
 
 app.listen(port, () => {
-  console.log(`App de exemplo esta rodando na porta ${port}`)
-})
+  console.log(`App de exemplo esta rodando na porta ${port}`);
+});
